3476a08e7fd5baed5c97a45a614b4a71cef0eb26
Kaoru Wada <hello@nagomu.me>
2025-12-26 07:21:22 +0900

    Add the example code

---
+++ sample01/vivliostyle.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: "Example 01 - Vivliostyle",
+  size: "JIS-B5",
+  entry: ["sample01/index.html"],
+  output: ["dist/sample01.pdf"],
+  workspaceDir: "sample01/.vivliostyle",
+};
